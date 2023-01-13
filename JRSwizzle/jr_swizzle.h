baaddf66eb3a93c2410254f5f833b37153884bb6
Alexander Kukla <alexander.kukla@gmail.com>
2023-01-13 17:11:13 +0200

    Create project for Carthage support

---
+++ JRSwizzle/jr_swizzle.h
@@ -0,0 +1,18 @@
+//
+//  JRSwizzle.h
+//  JRSwizzle
+//
+//  Created by Alexander Kukla on 13.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JRSwizzle.
+FOUNDATION_EXPORT double JRSwizzleVersionNumber;
+
+//! Project version string for JRSwizzle.
+FOUNDATION_EXPORT const unsigned char JRSwizzleVersionString[];
+
+import <JRSwizzle/JRSwizzle.h>
+
+
